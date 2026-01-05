4a1fb09d2ed1fbd65c3f3cad43097fd80690b6ec
Anastasiia Radasheva <eha20082@gmail.com>
2026-01-05 13:05:46 +0200

    uus funk on naitatabel

--- funk.php
+++ funk.php
@@ -7,3 +7,18 @@ function lisapunkt($id)
     $paring->execute();
     header("Location:" . $_SERVER['PHP_SELF']);//aadresiriba puhasta päring ja jääb failinimi
 }
+function naitatabel($id){
+
+    global $connect;
+    $paring=$connect->prepare("
+Select id, president, pilt, punktid, lisamisaeg  from valimused where avalik=1");
+    $paring->bind_result($id, $president, $pilt, $punktid, $lisamisaeg);
+    $paring->execute();
+    while($paring->fetch()){
+        echo "<tr>";
+        echo "<td>{$president}</td>";
+        echo "<td>{$punktid}</td>";
+        echo "<td><a href='?lisa1punktid={$id}'> +1 punkt</a></td>";
+        echo "<td><a href='?kustuta1punktid=$id'> -1 punkt</a></td>";
+        echo "</tr>";
+}
